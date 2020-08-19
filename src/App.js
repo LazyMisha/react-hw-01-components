2c87a72af547260ac6175af08805aaee26121569
misha <misha_trunov@hotmail.com>
2020-08-19 20:59:23 +0300

    hw 01 user profile

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Profile from "./components/Profile";
+import user from "./user.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Profile
+          avatar={user.avatar}
+          name={user.name}
+          tag={user.tag}
+          location={user.location}
+          stats={user.stats}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
