febac5d3a3c0d137e0767fd8d0b5a94289a368e6
misha <misha_trunov@hotmail.com>
2020-08-23 14:05:42 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-hw-01-components/precache-manifest.75c53864f9dc0cddeeacb1f5d7462233.js"
+  "/react-hw-01-components/precache-manifest.db43cc9fe8e5b9a5cf3c4b17cb46f96a.js"
 );
 
 self.addEventListener('message', (event) => {
