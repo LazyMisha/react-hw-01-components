5f14c734a9d1c15582e987036d363e1be7928c0d
misha <misha_trunov@hotmail.com>
2020-08-23 13:04:47 +0300

    Updates

---
+++ precache-manifest.75c53864f9dc0cddeeacb1f5d7462233.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f8a7d70cb4928b0c4cc7bce5231bb65",
+    "url": "/react-hw-01-components/index.html"
+  },
+  {
+    "revision": "e4e731bdc89523232dff",
+    "url": "/react-hw-01-components/static/css/main.4470d987.chunk.css"
+  },
+  {
+    "revision": "6e9e1040effa9a841221",
+    "url": "/react-hw-01-components/static/js/2.d0840bc4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-hw-01-components/static/js/2.d0840bc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4e731bdc89523232dff",
+    "url": "/react-hw-01-components/static/js/main.da7de72d.chunk.js"
+  },
+  {
+    "revision": "6fe3cc52a8b0257f05d9",
+    "url": "/react-hw-01-components/static/js/runtime-main.459b7fff.js"
+  }
+]);
